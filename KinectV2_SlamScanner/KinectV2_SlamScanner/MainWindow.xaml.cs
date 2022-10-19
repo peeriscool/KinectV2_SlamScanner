d762e7fdaa0dac9c24ce1f8110d8a65fab39de65
peeriscool <44834355+peeriscool@users.noreply.github.com>
2022-10-19 18:14:14 +0200

    Setup Refrences and dependicies and

--- KinectV2_SlamScanner/KinectV2_SlamScanner/MainWindow.xaml.cs
+++ KinectV2_SlamScanner/KinectV2_SlamScanner/MainWindow.xaml.cs
@@ -1,17 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.ComponentModel;
+using System.Globalization;
+using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Media.Media3D;
+using System.Windows.Threading;
+using Microsoft.Kinect;
+using Microsoft.Kinect.Fusion;
+using Wpf3DTools;
 
 namespace KinectV2_SlamScanner
 {
---
+++ KinectV2_SlamScanner_netframework4.5/SlamScanner/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿
+
+namespace SlamScanner
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Globalization;
+    using System.IO;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using System.Windows;
+    using System.Windows.Data;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using System.Windows.Media.Media3D;
+    using System.Windows.Threading;
+    using Microsoft.Kinect;
+    using Microsoft.Kinect.Fusion;
+    using Wpf3DTools;
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged, IDisposable
+    {
+        
+       // Microsoft.Kinect.KinectSensor
+        private KinectSensor sensor = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
